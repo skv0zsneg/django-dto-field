1dfaf999ba9aea98c0b92d0c9ec4b7a515e4e203
skv0zsneg <klimentlamonov@yandex.ru>
2025-09-18 01:04:30 +0300

    Refactor serializer for using msgpack.

--- src/django_dict_field/serializer.py
+++ src/django_dict_field/serializer.py
@@ -1,31 +1,11 @@
-import bz2
-import pickle
-from typing import Any, ClassVar, Generic, Optional, TypeVar
-
-P = TypeVar("P")
+import msgpack
 
 
-class Serializer(Generic[P]):
+class Serializer:
     """Params serializer & deserializer."""
 
-    ALLOWED_TYPES: ClassVar[tuple[Any, ...]] = (dict,)
+    def serialize(self, value: dict) -> bytes:
+        return msgpack.packb(value)
 
-    def __init__(self, params_type: Optional[P] = None) -> None:
-        self.params_type = params_type
-
-    def serialize(self, value: P) -> bytes:
-        serialized = pickle.dumps(value)
-        compressed = bz2.compress(serialized)
-        return compressed
-
-    def deserialize(self, value: bytes) -> P:
-        decompressed = bz2.decompress(value)
-        deserialized = pickle.loads(decompressed)
-        return deserialized
-
-    def is_valid(self, value: P) -> bool:
-        return any(
-            map(
-                lambda allowed_type: isinstance(value, allowed_type), self.ALLOWED_TYPES
-            )
-        )
+    def deserialize(self, value: bytes) -> dict:
+        return msgpack.unpackb(value)
