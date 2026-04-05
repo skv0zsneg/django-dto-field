34b509021b249a264f0b2cb0de0b67534fe2553d
skv0zsneg <klimentlamonov@yandex.ru>
2026-04-05 05:13:43 +0300

    Remove noqa on WPS error.

--- src/django_dto_field/parser.py
+++ src/django_dto_field/parser.py
@@ -48,4 +48,4 @@ class RawDtoParser:
     def get_serializer_code(cls, raw_dto: bytes) -> bytes:
         if len(raw_dto) < cls._header_size:
             raise CorruptedDtoError("Corrupted: Header DTO to short.")
-        return raw_dto[0:1]  # noqa: WPS349
+        return raw_dto[:1]
