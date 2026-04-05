b8dda95351fdd0fce6c27d84ed1b96f652ab971d
skv0zsneg <klimentlamonov@yandex.ru>
2026-04-05 04:07:05 +0300

    Add registry for serializer storing.

---
+++ src/django_dto_field/registry.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from django_dto_field.serializer import BaseDtoSerializer
+
+
+class Registry:
+    """Global registry for storing something."""
+
+    def __init__(self) -> None:
+        self._serializers: dict[bytes, type["BaseDtoSerializer"]] = {}
+
+    def save_serializer(self, code: bytes, serializer: type["BaseDtoSerializer"]) -> None:
+        self._serializers[code] = serializer
+
+    def get_serializer(self, code: bytes) -> type["BaseDtoSerializer"]:
+        return self._serializers[code]
+
+
+# TODO: Globals is bad. Need to do something with it.
+registry = Registry()
