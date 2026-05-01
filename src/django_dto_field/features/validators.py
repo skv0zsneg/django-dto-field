0d3a016a6ab4ccbb7cde95bdcc5ef3f36749ebbf
skv0zsneg <klimentlamonov@yandex.ru>
2026-05-01 23:35:31 +0300

    Add validation feature + dataclass validation.

---
+++ src/django_dto_field/features/validators.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from django_dto_field.exceptions import ValidatorError
+from django_dto_field.features.base import BaseDtoFeature, DtoCodeEnum
+
+T_DTO = TypeVar("T_DTO")
+
+
+class BaseDtoValidator(BaseDtoFeature, Generic[T_DTO], ABC):
+    """Interface for DTO validators."""
+
+    @abstractmethod
+    def validate(self, value_dto: T_DTO, schema: type[T_DTO]) -> None:
+        """Must implement validate method in child validators."""
+
+
+class DataclassValidator(BaseDtoValidator):
+    """Validator for `dataclass`."""
+
+    dto_code = DtoCodeEnum.DATACLASS
+
+    def validate(self, value_dto: T_DTO, schema: type[T_DTO]) -> None:
+        if not isinstance(value_dto, schema):
+            raise ValidatorError(
+                "Validation Error: given dataclass DTO is not match given schema %s" % schema
+            )
