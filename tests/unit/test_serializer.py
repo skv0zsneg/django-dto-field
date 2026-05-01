f64621da4ebb5a182f92264b97d7c77c5403457f
skv0zsneg <klimentlamonov@yandex.ru>
2026-05-01 23:34:48 +0300

    Fix test.

--- tests/unit/test_serializer.py
+++ tests/unit/test_serializer.py
@@ -51,7 +51,9 @@ class TestDictDtoSerializer:
 
 class TestBaseDtoSerializerConstraints:
     def test_abstract_class_cannot_be_instantiated(self) -> None:
-        with pytest.raises(TypeError, match="Can't instantiate abstract class"):
+        with pytest.raises(
+            DtoFeatureError, match="Dto Feature Error: missing definition of `dto_code`"
+        ):
             BaseDtoSerializer()
 
     def test_missing_dto_code_raises_on_init(self) -> None:
